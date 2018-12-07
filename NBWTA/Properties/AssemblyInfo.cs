bbc3d36695cff46a2106cf8a7ffc8752674dfd88
Vladimir Pavelka <pavelka.vlado@gmail.com>
2018-12-07 19:23:14 +0100

    nuspec

--- NBWTA/Properties/AssemblyInfo.cs
+++ NBWTA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NBWTA")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Brood War Terrain Analyzer written in .NET (choke points, regions detection)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NBWTA")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Vladimir Pavelka 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
