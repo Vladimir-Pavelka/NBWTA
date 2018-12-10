e6a6cc8d880b28b0637d3cefb91f207dfdd20a9c
Vladimir Pavelka <pavelka.vlado@gmail.com>
2018-12-10 02:36:34 +0100

    Added test for map offline bitmap analysis, Line tests

---
+++ Tests/LineTest.cs
@@ -0,0 +1,111 @@
+﻿namespace Tests
+{
+    using System.Drawing;
+    using System.Linq;
+    using FluentAssertions;
+    using NBWTA.Utils;
+    using Xunit;
+
+    public class LineTest
+    {
+        [Fact]
+        public void Horizontal_Line_Slope_Should_Be_Zero()
+        {
+            var line = Line.FromPoints(new Point(2, 3), new Point(4, 3));
+            var lineLeastSquares = Line.FromPoints(new[] { new Point(2, 3), new Point(4, 3) });
+
+            new[] { line, lineLeastSquares }.ToList().ForEach(l =>
+            {
+                line.Slope.Should().Be(0);
+                line.IsHorizontal.Should().BeTrue();
+            });
+        }
+
+        [Fact]
+        public void Directly_Proportional_Line_Should_Have_Slope_One_And_Offset_Zero()
+        {
+            var line = Line.FromPoints(new Point(1, 1), new Point(4, 4));
+            var lineLeastSquares = Line.FromPoints(new[] { new Point(1, 1), new Point(4, 4), new Point(7, 7), });
+
+            new[] { line, lineLeastSquares }.ToList().ForEach(l =>
+            {
+                line.Slope.Should().Be(1);
+                line.Offset.Should().Be(0);
+            });
+        }
+
+        [Fact]
+        public void Perpendicular_To_Directly_Proportional_Line_Should_Have_Slope_Minus_One()
+        {
+            var line = Line.FromPoints(new Point(1, 1), new Point(4, 4));
+            var perpendicular = line.GetPerpendicularAt(new Point(3, 3));
+
+            perpendicular.Slope.Should().Be(-1);
+            perpendicular.Offset.Should().Be(6);
+        }
+
+        [Fact]
+        public void Least_Squares_Approximated_Line_Should_Be_Calculated_Correctly()
+        {
+            var lineLeastSquares = Line.FromPoints(new[]
+                {new Point(1, 1), new Point(1, -1), new Point(7, 7), new Point(7, -7)});
+
+            lineLeastSquares.IsHorizontal.Should().BeTrue();
+            lineLeastSquares.Offset.Should().Be(0);
+        }
+
+        [Fact]
+        public void Perpendicular_ToHorizontal_Should_Be_Vertical()
+        {
+            var horizontal = Line.FromPoints(new Point(2, 4), new Point(5, 4));
+            var vertical = horizontal.GetPerpendicularAt(new Point(7, 4));
+            var convertedBack = vertical.GetPerpendicularAt(new Point(7, 4));
+
+            vertical.IsVertical.Should().BeTrue();
+            convertedBack.IsHorizontal.Should().BeTrue();
+            convertedBack.Offset.Should().Be(4);
+        }
+
+        [Fact]
+        public void Order_Of_Points_Does_Not_Matter()
+        {
+            var line1 = Line.FromPoints(new Point(1, 5), new Point(5, 1));
+            var line2 = Line.FromPoints(new Point(5, 1), new Point(1, 5));
+
+            line1.Slope.Should().Be(line2.Slope);
+            line1.Offset.Should().Be(line2.Offset);
+        }
+
+        [Fact]
+        public void Vertical_Line_Yields_Horizontal_Indices()
+        {
+            var horizontalLine = Line.FromPoints(new Point(1, 5), new Point(5, 5));
+            var verticalLine = Line.FromPoints(new Point(1, 5), new Point(1, 10));
+
+            horizontalLine.LeftIndexesFrom((3, 5)).Take(3).Should()
+                .BeEquivalentTo(new[] { new Point(2, 5), new Point(1, 5), new Point(0, 5) });
+            horizontalLine.RightIndexesFrom((3, 5)).Take(3).Should()
+                .BeEquivalentTo(new[] { new Point(4, 5), new Point(5, 5), new Point(6, 5) });
+
+            verticalLine.LeftIndexesFrom((1, 7)).Take(3).Should()
+                .BeEquivalentTo(new[] { new Point(1, 6), new Point(1, 5), new Point(1, 4) });
+            verticalLine.RightIndexesFrom((1, 7)).Take(3).Should()
+                .BeEquivalentTo(new[] { new Point(1, 8), new Point(1, 9), new Point(1, 10) });
+        }
+
+        [Fact]
+        public void Directly_Proportional_Line_Yields_Diagonal_Indices()
+        {
+            var directProportion = Line.FromPoints(new Point(1, 1), new Point(3, 3));
+            directProportion.LeftIndexesFrom((4, 4)).Take(7).Should().BeEquivalentTo(new[]
+            {
+                new Point(3, 3), new Point(3, 3), new Point(2, 2), new Point(1, 1), new Point(0, 0), new Point(0, 0), new Point(-1, -1)
+            });
+
+            directProportion.RightIndexesFrom((4, 4)).Take(7).Should().BeEquivalentTo(new[]
+            {
+                new Point(5, 5), new Point(5, 5), new Point(6, 6), new Point(7, 7), new Point(8, 8), new Point(8, 8), new Point(9, 9)
+            });
+        }
+    }
+}
---
+++ Tests/OfflineMapAnalysisTest.cs
@@ -0,0 +1,126 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Drawing;
+    using System.IO;
+    using System.Linq;
+    using NBWTA;
+    using NBWTA.Result;
+    using NBWTA.Utils;
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class OfflineMapAnalysisTest
+    {
+        private const string BwMapsFolder = "../../BwMaps";
+
+        private const string AstralBalance = BwMapsFolder + "/AstralBalance/AstralBalance_walls.bmp";
+        private const string CircuitBreaker = BwMapsFolder + "/CircuitBreaker/CircuitBreaker_walls.bmp";
+        private const string Hunters = BwMapsFolder + "/TheHunters/TheHunters_walls.bmp";
+        private const string LostTemple = BwMapsFolder + "/TheLostTemple/TheLostTemple_walls.bmp";
+        private const string FightingSpirit = BwMapsFolder + "/FightingSpirit/FightingSpirit_walls.bmp";
+        private const string Homeworld = BwMapsFolder + "/Homeworld/Homeworld_walls.bmp";
+        private const string Python = BwMapsFolder + "/iCCupPython1.3/iCCupPython1.3_walls.bmp";
+        private const string CrescentMoon = BwMapsFolder + "/CrescentMoon/CrescentMoon_walls.bmp";
+        private const string BloodBath = BwMapsFolder + "/BloodBath/BloodBath_walls.bmp";
+        private const string OrbitalGully = BwMapsFolder + "/OrbitalGully/OrbitalGully_walls.bmp";
+        private const string AlphaDraconis = BwMapsFolder + "/AlphaDraconisVolcanicWorld/AlphaDraconisVolcanicWorld_walls.bmp";
+        private const string Bottleneck = BwMapsFolder + "/Bottleneck/Bottleneck_walls.bmp";
+        private const string Heartwood = BwMapsFolder + "/Heartwood/Heartwood_walls.bmp";
+        private const string PrimevalIsles = BwMapsFolder + "/PrimevalIsles/PrimevalIsles_walls.bmp";
+        private const string RiverCrossing = BwMapsFolder + "/RiverCrossing/RiverCrossing_walls.bmp";
+
+        private const string ResultFolder = "../../MapAnalysisResults/";
+
+        private readonly ITestOutputHelper _console;
+
+        public OfflineMapAnalysisTest(ITestOutputHelper console)
+        {
+            _console = console;
+        }
+
+        [Theory]
+        [InlineData(nameof(AstralBalance), AstralBalance)]
+        [InlineData(nameof(CircuitBreaker), CircuitBreaker)]
+        [InlineData(nameof(Hunters), Hunters)]
+        [InlineData(nameof(LostTemple), LostTemple)]
+        [InlineData(nameof(FightingSpirit), FightingSpirit)]
+        //[InlineData(nameof(Homeworld), Homeworld)]
+        [InlineData(nameof(Python), Python)]
+        [InlineData(nameof(CrescentMoon), CrescentMoon)]
+        [InlineData(nameof(BloodBath), BloodBath)]
+        [InlineData(nameof(OrbitalGully), OrbitalGully)]
+        [InlineData(nameof(AlphaDraconis), AlphaDraconis)]
+        [InlineData(nameof(Bottleneck), Bottleneck)]
+        [InlineData(nameof(Heartwood), Heartwood)]
+        //[InlineData(nameof(PrimevalIsles), PrimevalIsles)]
+        [InlineData(nameof(RiverCrossing), RiverCrossing)]
+        public void AnalyzeBatch(string mapName, string mapFile)
+        {
+            AnalyzeSingle(mapName, mapFile);
+        }
+
+        private void AnalyzeSingle(string mapName, string mapFile)
+        {
+            var outputFileName = mapName + ".bmp";
+
+            var algorithm = new MapAnalyzer();
+            var inputMapBitmap = new Bitmap(Image.FromFile(mapFile));
+
+            var walkabilityMap = ToWalkabilityMap(inputMapBitmap);
+
+            AnalyzedMap result = null;
+            var elapsedMs = MeasureElapsedMs(() =>
+                result = algorithm.Analyze(inputMapBitmap.Width, inputMapBitmap.Height, tile => walkabilityMap[tile.x, tile.y]));
+
+            _console.WriteLine($"{mapName}: {elapsedMs}ms");
+
+            FillChokesIntoBitmap(result.ChokeRegions, inputMapBitmap);
+            FillRegionsIntoBitmap(result.MapRegions, inputMapBitmap);
+
+            Directory.CreateDirectory(ResultFolder);
+            inputMapBitmap.Save(ResultFolder + outputFileName);
+        }
+
+        private static long MeasureElapsedMs(Action act)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            act();
+            return stopwatch.ElapsedMilliseconds;
+        }
+
+        private static bool[,] ToWalkabilityMap(Bitmap inputBitmap)
+        {
+            var walkabilityMap = new bool[inputBitmap.Width, inputBitmap.Height];
+
+            for (var column = 0; column < inputBitmap.Width; column++)
+                for (var row = 0; row < inputBitmap.Height; row++)
+                    walkabilityMap[column, row] = IsWalkable(inputBitmap.GetPixel(column, row));
+
+            return walkabilityMap;
+        }
+
+        private static bool IsWalkable(Color color) =>
+            new[] { Color.White, Color.Transparent, Color.Empty }.Any(c => c.ToArgb() == color.ToArgb());
+
+        private static void FillChokesIntoBitmap(IEnumerable<ChokeRegion> chokeRegions, Bitmap bitmap) =>
+            chokeRegions.ForEach(x => x.ContentTiles.ForEach(n => bitmap.SetPixel(n.x, n.y, Color.Red)));
+
+        private static void FillRegionsIntoBitmap(IEnumerable<MapRegion> mapRegions, Bitmap bitmap) =>
+            mapRegions.Select((region, idx) => (region: region, color: GetPresetColor(idx)))
+                .ForEach(x => x.region.OutlineTiles.ForEach(n => bitmap.SetPixel(n.x, n.y, x.color)));
+
+        private static Color GetPresetColor(int idx)
+        {
+            var colorPresets = new[]
+            {
+                Color.Aqua, Color.Fuchsia, Color.Green, Color.Blue, Color.Lime, Color.Black,
+                Color.Orange, Color.Maroon, Color.Yellow, Color.Purple, Color.Teal
+            };
+
+            return colorPresets[idx % colorPresets.Length];
+        }
+    }
+}
