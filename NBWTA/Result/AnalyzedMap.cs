743080c660f2c4a0441a6eae1c773f3976c58dad
Karl Müller <msdn@sinc.de>
2019-09-16 17:57:44 +0200

    AnalyzedMap result can now provide BaseInfo based on Start location

--- NBWTA/Result/AnalyzedMap.cs
+++ NBWTA/Result/AnalyzedMap.cs
@@ -1,6 +1,8 @@
 ﻿namespace NBWTA.Result
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using ResultPersister;
 
     public class AnalyzedMap
@@ -9,12 +11,21 @@
         {
             MapRegions = mapRegions;
             ChokeRegions = chokeRegions;
+            ResourceRegions = mapRegions.Where(mr => mr.ResourceSites.Any()).ToList();
         }
 
         public IReadOnlyCollection<MapRegion> MapRegions { get; }
         public IReadOnlyCollection<ChokeRegion> ChokeRegions { get; }
+        public IReadOnlyCollection<MapRegion> ResourceRegions { get; }
+
+        public BaseInfo GetBaseInfoByStartLocationWalkTile((int x, int y) startLocationWalkTile)
+        {
+            var startLocationRegion = MapRegions.FirstOrDefault(r => r.ContentTiles.Contains(startLocationWalkTile));
+            if (startLocationRegion != null) return new BaseInfo(startLocationRegion);
+            throw new ArgumentException($"startLocationWalkTile: {startLocationWalkTile} was not found in any MapRegion", nameof(startLocationWalkTile));
+        }
 
         public void SaveToFile(string filePath) => FilePersister.Save(this, filePath);
         public static Result<AnalyzedMap> TryLoadFromFile(string filePath) => FilePersister.TryLoad(filePath);
     }
-}
+}   
---
+++ NBWTA/Result/BaseInfo.cs
@@ -0,0 +1,53 @@
+﻿namespace NBWTA.Result
+{
+    using NBWTA.Utils;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BaseInfo
+    {
+        public BaseInfo(MapRegion startLocationRegion)
+        {
+            StartLocationRegion = startLocationRegion;
+
+            NaturalExpansions = startLocationRegion.AdjacentChokes
+                .SelectMany(ch => ch.AdjacentRegions)
+                .Except(startLocationRegion.Yield())
+                .Where(r => r.ResourceSites.Any())
+                .ToList();
+
+            ChokesBetweenMainAndNaturals = startLocationRegion.AdjacentChokes
+                .Intersect(NaturalExpansions.SelectMany(n => n.AdjacentChokes))
+                .ToList();
+
+            var naturalsEntrances = NaturalExpansions
+                .SelectMany(n => n.AdjacentChokes)
+                .GroupBy(ch => ch).ToList();
+
+            OutsideEntrancesToNaturals = naturalsEntrances
+                .Where(chg => chg.Count() == 1)
+                .Select(chg => chg.Key)
+                .Except(ChokesBetweenMainAndNaturals)
+                .ToList();
+
+            InsideConnectorsBetweenNaturals = naturalsEntrances
+                .Where(chg => chg.Count() > 1)
+                .Select(chg => chg.Key)
+                .ToList();
+        }
+
+        /// <summary>
+        /// Region representing the main base start location
+        /// </summary>
+        public MapRegion StartLocationRegion { get; }
+
+        /// <summary>
+        /// MapRegions containing resources, adjacent to the StartLocationRegion
+        /// There can be 0, 1 or more NaturalExpansions, based on the map shape.
+        /// </summary>
+        public IReadOnlyCollection<MapRegion> NaturalExpansions { get; }
+        public IReadOnlyCollection<ChokeRegion> ChokesBetweenMainAndNaturals { get; }
+        public IReadOnlyCollection<ChokeRegion> OutsideEntrancesToNaturals { get; }
+        public IReadOnlyCollection<ChokeRegion> InsideConnectorsBetweenNaturals { get; }
+    }
+}
